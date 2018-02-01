6859aa5a584defba4b4b87b0e6f7aae9933d64e2
ddbrennan <ddbren@gmail.com>
2018-02-01 14:04:03 -0500

    kinda works i guess

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import HogContainer from './HogContainer'
 import Filter from './Filter'
 import hogs from '../porkers_data.js'
 
+const hogWeight = 'weight as a ratio of hog to LG - 24.7 Cu. Ft. French Door Refrigerator with Thru-the-Door Ice and Water'
 
 class App extends Component {
   constructor(props) {
@@ -21,32 +22,39 @@ class App extends Component {
     let greaseStatus = !this.state.greasy
     this.setState({
       greasy: greaseStatus
-    }, this.handleRenderHog())
+    }, this.handleRenderHog)
   }
 
-  handleRenderHog() {
+  handleRenderHog = () => {
     let hogArray = hogs
+    if (this.state.filter === "weight") {
+      hogArray = hogArray.sort((a,b) => a[hogWeight] - b[hogWeight])
+    } else if (this.state.filter === "name") {
+      hogArray = hogArray.sort((a,b) => a.name.localeCompare(b.name))
+    }
 
-    if (!this.state.greasy) {
+    if (this.state.greasy) {
       hogArray = hogArray.filter((h) => h.greased)
     }
+
     this.setState({
       currentHogs: hogArray
     })
-    console.log(hogArray.length)
   }
 
 
-  handleFilterChange() {
-
+  handleFilterChange = (e) => {
+    let newFilter = e.target.value
+    this.setState({
+      filter: newFilter
+    }, this.handleRenderHog)
   }
-
 
   render() {
     return (
       <div className="App">
         < Nav />
-        < Filter handleGreaseSelection = {this.handleGreaseSelection}/>
+      < Filter handleGreaseSelection = {this.handleGreaseSelection} handleFilterChange={ this.handleFilterChange }/>
         < HogContainer hogs={this.state.currentHogs}/>
       </div>
     )
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -11,8 +11,8 @@ class Filter extends React.Component {
         <label for="greasy">Greasy?
           <input type="checkbox" value="greasy" id="greasy" onChange={this.props.handleGreaseSelection}></input>
         </label>
-        <select>
-          <option disabled selected value>Sort By:</option>
+        <select onChange={ this.props.handleFilterChange }>
+          <option value="">All</option>
           <option value="name">Alphabetical</option>
           <option value="weight">Weight</option>
         </select>
